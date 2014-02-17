0c15bd74c771979a8397007664ad767b5e6642a7
Luanht <htluan2605@gmail.com>
2014-02-17 13:30:11 +0700

    fix permit signup params for user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  before_filter :configure_permitted_parameters, :only => [:create, :update]
+
   # New subscriber
   def create
     build_resource(build_subscriber_sign_up_params)
@@ -19,9 +21,22 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
+
   private
 
   def build_subscriber_sign_up_params
     sign_up_params.merge(:type => Subscriber.name)
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :birthday,
+               :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:first_name, :last_name, :birthday,
+               :password, :password_confirmation, :current_password)
+    end
+  end
+
 end
