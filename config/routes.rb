b002f9429ed86b10edf20f6a25cbdb9ce649fd2b
Luanht <htluan2605@gmail.com>
2014-02-17 10:39:42 +0700

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TipsterHero::Application.routes.draw do
+  root 'home#index'
+
   devise_for :users, :controllers => {
       :omniauth_callbacks => "users/omniauth_callbacks",
       :registrations => "users/registrations",
