735bf6476f9e454e6634be9673527aeedecd25b5
Quang <thanhquang1988@gmail.com>
2014-02-19 13:53:56 +0700

    Update code

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -4,13 +4,16 @@ class PaymentController < ApplicationController
   # POST /payment
   # Initialize and redirect to Paypal payment page
   def create
+    subscription = current_user.build_subscription(plan_id: session[:plan_id])
+    tipsters = Tipster.where(id: tipster_ids_in_cart)
+    subscription.tipsters << tipsters
+    subscription.save
     @plan = Plan.find session[:plan_id]
     @paypal_obj = Hash.new
-    @paypal_obj[:amount] = "%05.2f" % (@plan.price)
+    @paypal_obj[:amount] = "%05.2f" % (subscription.calculator_price)
     @paypal_obj[:currency] = "EUR"
     @paypal_obj[:item_number] = current_user.id
     @paypal_obj[:item_name] = "TipsterHero Subscriptions"
-    current_user.build_subscription(plan_id: session[:plan_id])
     render 'remote.js.haml'
   end
 
@@ -22,7 +25,7 @@ class PaymentController < ApplicationController
     if params[:pending_reason] != ''
       flash[:alert] = PAYPAL_PENDINGS["#{params[:pending_reason]}"]
     end
-    @params = params
+    @message = PAYPAL_PENDINGS["#{params[:pending_reason]}"]
   end
 
   # POST /payment/notify
@@ -33,8 +36,11 @@ class PaymentController < ApplicationController
     logger.info(notify)
    # require 'debugger';debugger
 #    notify.params['payment_status']
+    puts "ITEM ID #{notify.item_id}"
     user = User.find(notify.item_id)
-    subscription = user.build_subscription
+    subscription = user.subscription
+    payment = subscription.payments.build(payment_date: notify.params['payment_date'],payer_first_name: notify.params['first_name'],payer_last_name: notify.params['last_name'],payer_email: notify.params['payer_email'],residence_country: notify.params['residence_country'],pending_reason: notify.params['pending_reason'],mc_currency: notify.params['mc_currency'],business_email:  notify.params['business'],payment_type:  notify.params['payment_type'],payer_status:  notify.params['payer_status'],test_ipn:  notify.params['test_ipn'],tax:  notify.params['tax'],txn_id:  notify.params['txn_id'],receiver_email:  notify.params['receiver_email'],payer_id:  notify.params['payer_id'],receiver_id:  notify.params['receiver_id'],payment_status:  notify.params['payment_status'],mc_gross:  notify.params['mc_gross'])
+    payment.save
     # save payment data
     render nothing: true
   end
@@ -43,3 +49,4 @@ class PaymentController < ApplicationController
     redirect_to subscriptions_payment_path
   end
 end
+
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,10 @@
 class Subscription < ActiveRecord::Base
   has_many :tipsters , through: :subscriber_tipsters
+  belongs_to :plan
+  has_many :subscriber_tipsters
   belongs_to :user
+  has_many :payments
+  def calculator_price
+    return self.plan.price.to_f + self.tipsters.size * 9.9
+  end
 end
