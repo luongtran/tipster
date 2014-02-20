49548cbd8179ba83d89844ce055dec584aa886d6
Luanht <htluan2605@gmail.com>
2014-02-20 09:12:49 +0700

    refactor payment model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,9 +1,10 @@
 class Subscription < ActiveRecord::Base
-  has_many :tipsters , through: :subscriber_tipsters
+  has_many :tipsters, through: :subscriber_tipsters
   belongs_to :plan
   has_many :subscriber_tipsters
   belongs_to :user
   has_many :payments
+
   def calculator_price
     puts "PLAN PRICE #{self.plan.price} TIPSTER OFFER #{(self.tipsters.size - 1) * 9.9}"
     return self.plan.price.to_f + (self.tipsters.size - 1) * 9.9
