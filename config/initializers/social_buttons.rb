ec10dd03270f045482338f99af2f33fbbbf3ace3
Luanht <htluan2605@gmail.com>
2014-02-21 11:07:19 +0700

    completed generate coupon code

--- config/initializers/social_buttons.rb
+++ config/initializers/social_buttons.rb
@@ -1,6 +1,8 @@
-SocialButtons.config :tweet do |tweet|
+# config/initializers/social_buttons.rb
+SocialButtons.config do |social|
   social.tweet do |tweet|
     tweet.default_options = {:via => "@tipsterhero"}
   end
+
   #social.like.default_options = {:via => "myself"}
 end
