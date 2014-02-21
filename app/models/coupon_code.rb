340fd0eda6ed4dbe00b9a38b19ff95e871871eb0
Luanht <htluan2605@gmail.com>
2014-02-21 08:40:46 +0700

    rename coupon code model

--- app/models/coupon_code.rb
+++ app/models/coupon_code.rb
@@ -1,9 +1,14 @@
 class CouponCode < ActiveRecord::Base
 
-  SOURCE = [FACEBOOK = 'facebook', TWITTER = 'twitter']
+  SOURCES = [FACEBOOK = 'facebook', TWITTER = 'twitter']
 
   belongs_to :user
 
+  validates :user, :code, presence: true
+
+  # ==============================================================================
+  # CLASS METHODS
+  # ==============================================================================
   class << self
     def create_for_user(user, source = nil)
       cc = new(
@@ -17,10 +22,13 @@ class CouponCode < ActiveRecord::Base
     def generate_unique_code(user)
       Digest::MD5.hexdigest("#{user.id} #{Time.now.to_i} #{Time.now.usec}")
     end
-
   end
 
+  # ==============================================================================
+  # INSTANCE METHODS
+  # ==============================================================================
   def mark_used
     self.update_attributes is_used: true, used_at: DateTime.now
   end
+
 end
