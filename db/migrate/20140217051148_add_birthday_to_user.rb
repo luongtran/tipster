3aefbd62ea07044c395814f93ca21d78f9aea0df
Luanht <htluan2605@gmail.com>
2014-02-22 12:08:44 +0700

    clean up migrations

--- db/migrate/20140217051148_add_birthday_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddBirthdayToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :birthday, :date , :null => false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(version: 20140221104030) do
 
   create_table "authorizations", force: true do |t|
-    t.integer  "user_id"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "avatar_url"
+    t.integer "user_id"
+    t.string "provider"
+    t.string "uid"
+    t.string "avatar_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -25,32 +25,27 @@ ActiveRecord::Schema.define(version: 20140221104030) do
   add_index "authorizations", ["uid"], name: "index_authorizations_on_uid", using: :btree
   add_index "authorizations", ["user_id"], name: "index_authorizations_on_user_id", using: :btree
 
-  create_table "carts_tipsters", force: true do |t|
-    t.integer "cart_id"
-    t.integer "tipster_id"
-  end
-
   create_table "coupon_codes", force: true do |t|
-    t.integer  "user_id"
-    t.string   "code",                       null: false
-    t.string   "source"
-    t.boolean  "is_used",    default: false
+    t.integer "user_id"
+    t.string "code", null: false
+    t.string "source"
+    t.boolean "is_used", default: false
     t.datetime "used_at"
-    t.datetime "created_at",                 null: false
+    t.datetime "created_at", null: false
   end
 
   add_index "coupon_codes", ["user_id"], name: "index_coupon_codes_on_user_id", using: :btree
 
   create_table "invoices", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "amount",         default: 1
-    t.string   "currency"
-    t.string   "token"
-    t.string   "transaction_id"
-    t.string   "payer_id"
-    t.boolean  "completed",      default: false
-    t.boolean  "canceled",       default: false
-    t.integer  "payment_type"
+    t.integer "user_id"
+    t.integer "amount", default: 1
+    t.string "currency"
+    t.string "token"
+    t.string "transaction_id"
+    t.string "payer_id"
+    t.boolean "completed", default: false
+    t.boolean "canceled", default: false
+    t.integer "payment_type"
     t.datetime "created_at"
     t.datetime "completed_at"
   end
@@ -58,64 +53,64 @@ ActiveRecord::Schema.define(version: 20140221104030) do
   add_index "invoices", ["user_id"], name: "index_invoices_on_user_id", using: :btree
 
   create_table "payments", force: true do |t|
-    t.integer  "subscription_id"
+    t.integer "subscription_id"
     t.datetime "payment_date"
-    t.string   "payer_first_name"
-    t.string   "payer_last_name"
-    t.string   "payer_email"
-    t.string   "residence_country"
-    t.string   "pending_reason"
-    t.string   "mc_currency"
-    t.string   "business_email"
-    t.string   "payment_type"
-    t.string   "payer_status"
-    t.boolean  "test_ipn"
-    t.float    "tax"
-    t.string   "txn_id"
-    t.string   "receiver_email"
-    t.string   "payer_id"
-    t.string   "receiver_id"
-    t.string   "payment_status"
-    t.float    "mc_gross"
+    t.string "payer_first_name"
+    t.string "payer_last_name"
+    t.string "payer_email"
+    t.string "residence_country"
+    t.string "pending_reason"
+    t.string "mc_currency"
+    t.string "business_email"
+    t.string "payment_type"
+    t.string "payer_status"
+    t.boolean "test_ipn"
+    t.float "tax"
+    t.string "txn_id"
+    t.string "receiver_email"
+    t.string "payer_id"
+    t.string "receiver_id"
+    t.string "payment_status"
+    t.float "mc_gross"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "coupon_code_id"
+    t.integer "coupon_code_id"
   end
 
   add_index "payments", ["coupon_code_id"], name: "index_payments_on_coupon_code_id", using: :btree
   add_index "payments", ["subscription_id"], name: "index_payments_on_subscription_id", using: :btree
 
   create_table "plans", force: true do |t|
-    t.string   "name"
-    t.string   "title"
-    t.integer  "reception_delay",        default: 3600
-    t.text     "description"
-    t.boolean  "pause_ability",          default: true
-    t.boolean  "switch_tipster_ability", default: true
-    t.boolean  "profit_guaranteed",      default: true
-    t.float    "discount",               default: 0.0
-    t.float    "price"
-    t.integer  "number_tipster"
+    t.string "name"
+    t.string "title"
+    t.integer "reception_delay", default: 3600
+    t.text "description"
+    t.boolean "pause_ability", default: true
+    t.boolean "switch_tipster_ability", default: true
+    t.boolean "profit_guaranteed", default: true
+    t.float "discount", default: 0.0
+    t.float "price"
+    t.integer "number_tipster"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "period"
+    t.integer "period"
   end
 
   create_table "profiles", force: true do |t|
-    t.integer  "user_id",                                            null: false
-    t.string   "civility"
-    t.date     "birthday"
-    t.string   "address"
-    t.string   "city"
-    t.string   "country"
-    t.string   "zip_code"
-    t.string   "mobile_phone"
-    t.string   "telephone"
-    t.string   "favorite_betting_website"
-    t.string   "know_website_from"
-    t.integer  "secret_question"
-    t.string   "answer_secret_question"
-    t.boolean  "received_information_from_partners", default: false
+    t.integer "user_id", null: false
+    t.string "civility"
+    t.date "birthday"
+    t.string "address"
+    t.string "city"
+    t.string "country"
+    t.string "zip_code"
+    t.string "mobile_phone"
+    t.string "telephone"
+    t.string "favorite_betting_website"
+    t.string "know_website_from"
+    t.integer "secret_question"
+    t.string "answer_secret_question"
+    t.boolean "received_information_from_partners", default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -123,8 +118,8 @@ ActiveRecord::Schema.define(version: 20140221104030) do
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "subscriber_tipsters", force: true do |t|
-    t.integer  "tipster_id"
-    t.integer  "subscription_id"
+    t.integer "tipster_id"
+    t.integer "subscription_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -132,8 +127,8 @@ ActiveRecord::Schema.define(version: 20140221104030) do
   add_index "subscriber_tipsters", ["subscription_id"], name: "index_subscriber_tipsters_on_subscription_id", using: :btree
 
   create_table "subscriptions", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "plan_id"
+    t.integer "user_id"
+    t.integer "plan_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -142,28 +137,28 @@ ActiveRecord::Schema.define(version: 20140221104030) do
   add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "type",                                null: false
+    t.string "type", null: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
