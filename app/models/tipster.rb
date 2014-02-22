266ed5a2346043c1fd1a0c7b23e045592c1498db
Luanht <htluan2605@gmail.com>
2014-02-22 08:32:54 +0700

    move tipsters generating to rake task

--- app/models/tipster.rb
+++ app/models/tipster.rb
@@ -1,19 +1,7 @@
 class Tipster < User
 
   class << self
-    def initial_some_tipsters # Just for testing
-      5.times do
-        fn = Faker::Name.first_name
-        tst = new(
-            first_name: fn,
-            last_name: Faker::Name.last_name,
-            email: "#{fn}@gmail.com",
-            password: "123456",
-            password_confirmation: "123456"
-        )
-        tst.save!
-      end
-    end
+
   end
 
 end
