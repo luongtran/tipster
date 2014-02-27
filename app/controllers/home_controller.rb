dc79161b013edc879662d9888891c4781c16ac3d
Quang <thanhquang1988@gmail.com>
2014-02-27 16:46:21 +0700

    Update code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,13 +6,19 @@ class HomeController < ApplicationController
   def xml_view
     require 'open-uri'
     doc = Nokogiri::XML(open("http://xml.pinnaclesports.com/pinnacleFeed.aspx?sportType=soccer"))
-
-    Hash.from_xml(doc)["events"]["event"].inject({}) do |result, elem|
-      result[elem["name"]] = elem["value"]
-      result
+    events = doc.xpath("//event")
+    @results = []
+    events.each do |evt|
+      eve = {}
+      eve["datetime"] = evt.xpath("event_datetimeGMT").text
+      eve["league"] = evt.xpath("league").text
+      eve["islive"] = evt.xpath("IsLive").text
+      evt.xpath("participants//participant").each_with_index do |part,index|
+        eve["participiant_name_#{index}"] = part.xpath("participant_name").text
+        eve["visit_#{index}"] = part.xpath("visiting_home_draw").text
+      end
+      @results.push(eve)
     end
-    @result = result
-    render layout: false
   end
 
   def pricing
