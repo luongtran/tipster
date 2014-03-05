08989507b6e23ab888b4127f860d80b955cbf46f
Luanht <htluan2605@gmail.com>
2014-03-05 15:50:02 +0700

    fix font path

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -2,39 +2,41 @@
  = require highcharts
  */
 $(function () {
-    $('#profil-chart-container').highcharts({
-        title: {
-            text: '',
-            x: -20 //center
-        },
-        xAxis: {
-            categories: ['Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug']
-        },
-        yAxis: {
+    if ($('#profil-chart-container').length != 0) {
+        $('#profil-chart-container').highcharts({
             title: {
-                text: 'Bankroll'
+                text: '',
+                x: -20 //center
             },
-            plotLines: [
+            xAxis: {
+                categories: ['Mar', 'Apr', 'May', 'Jun',
+                    'Jul', 'Aug']
+            },
+            yAxis: {
+                title: {
+                    text: 'Bankroll'
+                },
+                plotLines: [
+                    {
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }
+                ]
+            },
+            tooltip: {
+                valueSuffix: ' units'
+            },
+            legend: false,
+            series: [
                 {
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
+                    name: 'Profit',
+                    data: [80, 120, 170, 180, 195, 190]
                 }
-            ]
-        },
-        tooltip: {
-            valueSuffix: ' units'
-        },
-        legend: false,
-        series: [
-            {
-                name: 'Profit',
-                data: [80, 120, 170, 180, 195, 190]
+            ],
+            credits: {
+                enabled: false
             }
-        ],
-        credits: {
-            enabled: false
-        }
-    });
+        });
+    }
 });
