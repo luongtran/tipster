c9ef852b909d407d0e435fa09e3c7ba8658aa85c
Luanht <htluan2605@gmail.com>
2014-03-05 18:33:29 +0700

    declaring bettypes

---
+++ app/models/bet_type.rb
@@ -0,0 +1,71 @@
+class BetType
+  # BetType::TYPES[:football].first[:code]
+  TYPES = {
+      football: [
+          {
+              code: 'over_under',
+              name: 'Over/Under',
+              other_name: 'Total goals',
+              definition: '...',
+              example: 'Chelsea - Manchester City. Bet on: Over +2.5',
+              selections: [
+                  'Over',
+                  'Under'
+              ],
+              line: {
+                  min: 0.5,
+                  max: 10,
+                  step: 0.25
+              }
+          },
+          {
+              code: 'match_odds',
+              name: 'Match odds',
+              other_name: '1X2;Three ways',
+              definition: '...',
+              example: 'Chelsea - Manchester City. Bet on: Chelsea',
+              selections: 'team',
+              drawable: true
+          },
+          {
+              code: 'asian_handicap',
+              name: 'Asian Handicap',
+              other_name: '',
+              definition: '...',
+              example: 'Chelsea - Manchester City. Bet on: Chelsea -1.25',
+              selections: 'team',
+              drawable: false,
+              line: {
+                  min: -10.0,
+                  max: 10.0,
+                  step: 0.25
+              }
+          }
+      ],
+      horse: [
+          {
+              code: 'winner',
+              name: 'Winner',
+              other_name: '1X2;Three ways',
+              definition: '...',
+              example: 'Lady melodie',
+              selections: 'team',
+              drawable: false
+
+          },
+          {
+
+          },
+          {
+
+          },
+          {
+
+          },
+          {
+
+          },
+      ]
+  }
+
+end
--- app/models/registration_service.rb
+++
@@ -1,5 +0,0 @@
-class RegistrationService
-  def create_subscriber(subscriber_params)
-    Subscriber.create(subscriber_params)
-  end
-end
