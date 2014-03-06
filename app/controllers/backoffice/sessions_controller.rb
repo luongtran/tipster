08f31a4f4d3a0b62cddf58c3d45c6218898a4391
Luanht <htluan2605@gmail.com>
2014-03-06 12:03:20 +0700

    allow only tipster account signin to backoffice

--- app/controllers/backoffice/sessions_controller.rb
+++ app/controllers/backoffice/sessions_controller.rb
@@ -1,5 +1,16 @@
 class Backoffice::SessionsController < SessionsController
   before_action :tipster_required
+
+  def create
+    super do
+      unless resource.rolable.is_a? Tipster
+        sign_out resource
+        flash.clear
+        flash[:alert] = I18n.t('devise.failure.not_found_in_database')
+      end
+    end
+  end
+
   protected
   def after_sign_in_path_for(resource)
     backoffice_root_path
