7c57eb1a874cb535331ba2f3416fae2181cdfabb
Luanht <htluan2605@gmail.com>
2014-03-07 09:07:42 +0700

    add missing infor to subscriber

--- app/controllers/subscribers/profile_controller.rb
+++ app/controllers/subscribers/profile_controller.rb
@@ -4,7 +4,7 @@ class Subscribers::ProfileController < ProfileController
   protected
   def user_params
     params.require(:user).permit(
-        :first_name, :last_name, :civility, :birthday, :address, :city, :country, :zip_code, :mobile_phone,
+        :first_name, :last_name, :nickname, :gender, :receive_tip_methods, :civility, :birthday, :address, :city, :country, :zip_code, :mobile_phone,
         :telephone, :favorite_beting_website, :know_website_from, :secret_question, :answer_secret_question, :receive_info_from_partners
     )
   end
