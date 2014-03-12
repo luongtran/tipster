8e41190036555347584a52a80899b02d08b154fb
Quang <thanhquang1988@gmail.com>
2014-03-12 16:48:02 +0700

    updated

--- db/seed_bettype.rb
+++
@@ -1,9 +0,0 @@
-BetType.delete_all
-BetType.create!({
-  :sport_id => 1, #Football
-  :code => "MATCHODDS",
-  :name => "Match odds",
-  :other_name => "",
-  :definition => "",
-  :example => ""
-    })
