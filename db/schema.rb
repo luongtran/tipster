527c2d221e378c16e501373f8dbba0bb1bd4f98a
Quang <thanhquang1988@gmail.com>
2014-03-24 16:18:05 +0700

    _subcription_show_display_

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140324065820) do
+ActiveRecord::Schema.define(version: 20140324065821) do
 
   create_table "accounts", force: true do |t|
     t.integer  "rolable_id"
