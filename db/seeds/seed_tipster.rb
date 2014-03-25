d3efa33dd9903d61395b3eb51a674eecea197666
Luanht <htluan2605@gmail.com>
2014-03-25 16:15:11 +0700

    update seed tipster

--- db/seeds/seed_tipster.rb
+++ db/seeds/seed_tipster.rb
@@ -5,7 +5,7 @@ if sports.empty?
 else
   ActiveRecord::Base.record_timestamps = false
   puts "\n===> Creating tipsters ==================="
-  25.times do
+  5.times do
     created_at = rand(100..222).days.ago - rand(1..15).hours
     fn = Faker::Name.first_name
     tipser = Tipster.new(
@@ -20,14 +20,16 @@ else
         updated_at: created_at
     )
     tipser.account.skip_confirmation!
-    tipser.save
-    # Add sport for tipster
-    # Any tipster have football
-    football = Sport.find_by(name: 'sport')
-    tipser.sports << football
-    sports_of_tipster = sports.sample(rand(2..3))
-    tipser.sports << sports_of_tipster
-    puts " -> Created tipster: #{tipser.full_name} -> sports: #{tipser.sports.map { |s| s.name.titleize }.join(',')}"
+    if tipser.save
+      # Add sport for tipster
+      # Any tipster have football
+      football_sport = Sport.find_by(name: 'football')
+      tipser.sports << football_sport if football_sport
+      sports_of_tipster = sports.sample(rand(2..3))
+      tipser.sports << sports_of_tipster
+      puts " -> Created tipster: #{tipser.full_name} -> sports: #{tipser.sports.map { |s| s.name.titleize }.join(',')}"
+    end
+
   end
   puts "\n===> Done tipsters seeds ==================="
   ActiveRecord::Base.record_timestamps = true
