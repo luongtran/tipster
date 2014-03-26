08bf3c764443c30b50fc553902c7f4eec4f2f3ae
luanht <htluan2605@gmail.com>
2014-03-27 00:20:17 +0700

    intitial more admin views

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,13 @@
+/*
+ = require jquery
+ = require jquery.turbolinks
+ = require jquery_ujs
+ = require_tree ../../../vendor/assets/javascripts/framework/bootstrap
+ = require helper
+ = require select2
+ = require highcharts/highcharts
+ = require bootsy
+ = require common
+ = require ajaxify
+ = require turbolinks
+ */
