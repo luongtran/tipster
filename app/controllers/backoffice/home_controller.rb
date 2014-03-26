17626ff7403a6b4d3ad4b8adf96f9cd7d94b8f77
Luanht <htluan2605@gmail.com>
2014-03-26 16:58:29 +0700

    add my statistics backoffice

--- app/controllers/backoffice/home_controller.rb
+++ app/controllers/backoffice/home_controller.rb
@@ -8,8 +8,11 @@ class Backoffice::HomeController < ApplicationController
   def dashboard
     @tipster = current_tipster.get_statistics
     @recent_tips = @tipster.tips.recent
-
-    @subscribers =  @tipster.followers
+    @subscribers = @tipster.followers
     @chart = Tipster.profit_chart_for_tipster(@tipster)
+  end
+
+  def my_statistics
+    @monthly_statistics = current_tipster.get_monthly_statistics
   end
 end
