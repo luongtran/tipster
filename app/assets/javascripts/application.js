f87155a8c44bada2f7a2e531e3f2643e0349215d
Luanht <htluan2605@gmail.com>
2014-04-07 19:30:46 +0700

    revert auto hide flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 
  = require jquery
  = require jquery_ujs
+ = require flash_message.js
  = require dev
  = require ajaxify
  = require helper
--- app/models/tip_journal.rb
+++ app/models/tip_journal.rb
@@ -1,9 +1,9 @@
 class TipJournal < ActiveRecord::Base
 
   # <Tipster> has created a new tip: <Match name> at <time>
-  # <Admin x> has approved and published a tip at>
-  # <Admin y> has rejected the tip <Match name>
-  # <Admin y> has finished the tip <Match name>
+  # <Admin x> has approved and published a tip at <time>
+  # <Admin y> has rejected the tip <Match name> at <time>
+  # <Admin y> has finished the tip <Match name> at <time>
 
   EVENT_CREATED = 'created'
   EVENT_REJECTED = 'rejected'
